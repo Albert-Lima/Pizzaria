c9622ae9c410a6268f0d8144966de6580f030c3a
Francisco Albert <albertsousalima@gmail.com>
2023-12-25 22:41:20 -0300

    Atualizei
    Projeto terminado.

--- arquivos/script.js
+++ arquivos/script.js
@@ -1,9 +1,55 @@
 var bttShowCardapio = document.querySelector('#ShowCardapio')
 var bttCloseCardápio = document.querySelector('#bttCloseCardápio')
 var BttRedCard = document.querySelector('#BttRedCard')
+var bttMenu = document.querySelector('#BttMenu')
+var bttPedido = document.querySelector('#pedidoBTT')
+var bttSobreNós = document.querySelector('#Normal1')
+var bttCardápio = document.querySelector('#Normal2')
+var abaMenu = document.querySelector('#AbaMenu')
 var Cardapio = document.querySelector('#cardapio')
 var Opções = document.querySelector('#Opções')
 var LinhaBase = document.querySelector('#Especiais')
+var AbaWho = document.querySelector('#História')
+
+bttSobreNós.addEventListener('click', ()=>{
+    var elementoRect1 = AbaWho.getBoundingClientRect()
+    var centerY1 = elementoRect1.top + elementoRect1.height/ 2 - window.innerHeight/ 2
+    window.scrollTo({
+        top: centerY1 + window.scrollY,
+        behavior: 'smooth'
+    })
+})
+bttCardápio.addEventListener('click', ()=>{
+    var elementoRect1 = Cardapio.getBoundingClientRect()
+    var centerY1 = elementoRect1.top + elementoRect1.height/ 2 - window.innerHeight/ 2
+    window.scrollTo({
+        top: centerY1 + window.scrollY,
+        behavior: 'smooth'
+    })
+})
+
+
+
+
+
+
+
+bttMenu.addEventListener('click', ()=>{
+    abaMenu.classList.toggle('BttMenu1')
+    abaMenu.classList.toggle('BttMenu2')
+
+    bttMenu.classList.toggle('OpCloseMenu1')
+    bttMenu.classList.toggle('OpCloseMenu2')
+
+    bttCardápio.classList.toggle('bttMenus1')
+    bttCardápio.classList.toggle('bttMenus2')
+
+    bttSobreNós.classList.toggle('bttMenus1')
+    bttSobreNós.classList.toggle('bttMenus2')
+
+    bttPedido.classList.toggle('bttMenus1')
+    bttPedido.classList.toggle('bttMenus2')
+})
 
 bttShowCardapio.addEventListener('click', ()=>{
     Cardapio.classList.toggle('Cardápio1-1')
